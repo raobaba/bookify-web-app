4b3e3b1221d8d07245d88aa829e023d21eb150f4
Rajan Kumar <raorajan9576@gmail.com>
2024-04-29 01:29:36 +0530

    navbar and listing page created

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/Register";
 import LoginPage from "./pages/Login";
+import MyNavbar from "./components/Navbar";
+import ListingPage from "./pages/List";
 
 function App() {
   return (
-    <Routes>
+    <div>
+      <MyNavbar/>
+      <Routes>
       <Route path="/" element={<h1>Home</h1>} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/book/list" element={<ListingPage />} />
     </Routes>
+    </div>
   );
 }
 
